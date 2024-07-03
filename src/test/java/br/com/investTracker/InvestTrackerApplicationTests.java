69475050134e64df1769972962f4ad377dbe93e4
Natália Diotto <diottonatalia@gmail.com>
2024-07-03 19:55:59 -0300

    Initial commit

---
+++ src/test/java/br/com/investTracker/InvestTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.investTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
