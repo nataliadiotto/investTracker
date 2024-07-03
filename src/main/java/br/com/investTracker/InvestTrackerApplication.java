fadebbdacf9870a76d5ce623ad774009f9ab1e15
Natália Diotto <diottonatalia@gmail.com>
2024-07-03 20:56:34 -0300

    feat: implement models

--- src/main/java/br/com/investTracker/InvestTrackerApplication.java
+++ src/main/java/br/com/investTracker/InvestTrackerApplication.java
@@ -1,13 +1,20 @@
 package br.com.investTracker;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InvestTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InvestTrackerApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
