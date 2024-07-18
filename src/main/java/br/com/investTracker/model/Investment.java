601373f0f8ed6e7736e7ec18ba095dc43bfd5bd1
Natália Diotto <diottonatalia@gmail.com>
2024-07-18 20:10:52 -0300

    feat: Investment's validations and error responses

--- src/main/java/br/com/investTracker/model/Investment.java
+++ src/main/java/br/com/investTracker/model/Investment.java
@@ -1,6 +1,8 @@
 package br.com.investTracker.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,8 +16,11 @@ public class Investment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "{ticker.not.blank}")
     private String ticker;
+    @NotNull(message = "{quantity.not.null}")
     private Integer quantity;
+    @NotNull(message = "{buyingPrice.not.null}")
     private BigDecimal buyingPrice;
     private BigDecimal sellingPrice;
 
