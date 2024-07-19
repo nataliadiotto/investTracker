c58a0948f9d040d4a3ecceda46c1f35d1b07bfb9
Natália Diotto <diottonatalia@gmail.com>
2024-07-18 22:03:26 -0300

    feat: query by ticker

--- src/main/java/br/com/investTracker/repository/InvestmentRepository.java
+++ src/main/java/br/com/investTracker/repository/InvestmentRepository.java
@@ -4,7 +4,10 @@ import br.com.investTracker.model.Investment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface InvestmentRepository extends JpaRepository<Investment, Long> {
 
+    List<Investment> findByTicker(String ticker);
 }
